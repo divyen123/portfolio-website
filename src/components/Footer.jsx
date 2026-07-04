50cfa5d056506d58aa571fdbd7224f9a06fb6f39
divyen123 <venileela2018@gmail.com>
2026-07-04 14:04:25 +0530

    style: shift footer line and contents slightly upwards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="border-t border-white/7 px-0 pt-8 pb-6 sm:px-16 sm:pb-19 lg:px-1 lg:pb-1">
+    <footer className="border-t border-white/7 mt-[-1.5rem] md:mt-[-3rem] px-0 pt-4 pb-4 sm:px-16 sm:pb-6 lg:px-1">
       <div className="mx-auto flex max-w-7xl flex-col items-center gap-6 text-center sm:flex-row sm:justify-between sm:text-left">
         <p className="text-sm text-slate-400">
           Copyright {year} {personalInfo.name}. All rights reserved.
